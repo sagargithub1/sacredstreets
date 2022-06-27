93460400ce900c095b94730d3d83628ecb55c9e4
SagarSharma <95302588+sagargithub1@users.noreply.github.com>
2022-06-27 23:43:07 +0530

    Add files via upload

---
+++ sacredstreets/about.php
@@ -0,0 +1,25 @@
+<?php include'header.php' ?>
+<section>
+    <!-- Social -->
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-12 col-sm-12" style="margin-top:100px;">
+                <h1 class="text-center" style="color:#7f89e4;">About Sacred Streets</h1>
+                <p class="text-center text-justify"><b>Welcome to my blog which is based on the holy city Varanasi,India
+                        which is situated in Uttar Pradesh(EAST), India.</b>
+                    <br> I will help in every possible ways to know much more about Varanasi, site contains many types
+                    of categories like History, Streets foods, Ghats, Specialities, etc.
+                    <b>Varanasi</b> is a city in the northern Indian state of Uttar Pradesh dating to the 11th century
+                    B.C. Regarded as the spiritual capital of India, the city draws Hindu pilgrims who bathe in the
+                    Ganges River’s sacred waters and perform funeral rites.
+                </p>
+                <p>In recent years the Ganges River has drawn attention for its ungodly level of pollution. ... Nearly
+                    2.5 million of them come each year to Varanasi, this holiest of cities, on the banks of the most
+                    sacred of Indian rivers. According to Hindu legend, Lord Shiva unleashed the Ganges from the knot of
+                    his hair.</p>
+            </div>
+        </div>
+        
+    </div>
+</section>
+<?php include'footer.php' ?>
